e6975bb6ee41edb02350583a4d87facae439969f
mojiyama <73383373+mojiyama@users.noreply.github.com>
2021-04-09 23:14:31 +0900

    Add files via upload

---
+++ lgtm/core.py
@@ -0,0 +1,13 @@
+import click
+
+@click.command()
+@click.option('--message','-m',default='LGTM',show_default=True,help='画像に乗せる文字')
+@click.argument('keyword')
+def cli(keyword,message):
+    "LGTM画像生成ツール"
+    lgtm(keyword,message)
+    click.echo('lgtm') #動作確認用
+
+def lgtm(keyword,message):
+    #ここにロジックを追加していく
+    pass
---
+++ lgtm/image_source.py
@@ -0,0 +1,24 @@
+import requests
+from io import BytesIO
+
+class LocalImage:
+    """ファイルから画像を取得する"""
+
+    def __init__(self,path):
+        self._path = path
+    
+    def get_image(self):
+        return open(self._path,'rb')
+
+class RemoteImage:
+    """URLから画像を取得する"""
+
+    def __init__(self,path):
+        self._url = path
+
+    def get_image(self):
+        data = requests.get(self._url)
+        #バイトデータをファイルオブジェクトに変換
+        return BytesIO(data.content)
+
+class _
